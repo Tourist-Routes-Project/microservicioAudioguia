3477806efa9d1ab60e75f2f52a1433cbd65ab6b6
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2025-01-11 12:32:54 +0100

    Añadiendo configuracion conexion mongo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+//Instancia de Expresss
 const app = express();
 
 require("dotenv").config();
@@ -12,3 +13,21 @@ mongoose.set("strictQuery", false);
 app.use(cors());
 app.use(express.json());
 
+//Funcion para conectar a la base de datos
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGO_CNN);
+    console.log(`El servidor está escuchando en el puerto ${process.env.PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//Llamamos a la función
+main().catch((error) => console.log(error));
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `El servidor está en funcionamiento en el puerto ${process.env.PORT}`
+  );
+});
